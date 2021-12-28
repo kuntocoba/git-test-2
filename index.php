c37850cff6627c3faf8bc7698cd35ca3e135804d
kunto <kunto.g4nteng@gmail.com>
2021-12-28 15:00:05 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat fugit voluptatibus asperiores suscipit maxime excepturi eum quia modi accusantium esse?</p>
 </body>
 </html>
